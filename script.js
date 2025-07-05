b951ef84aab64cc6c65a6f7011a9c3a3c9100de3
Pratiksha Pawar <pratikshapawar920@gmail.com>
2025-07-05 16:12:21 +0530

    first commit

--- script.js
+++ script.js
@@ -3,9 +3,15 @@ const totalPriceDisplay = document.getElementById('total-price');
 
 cards.forEach(card => {
   const radio = card.querySelector('input[type="radio"]');
+
   radio.addEventListener('change', () => {
+    // Remove 'expanded' from all cards
     cards.forEach(c => c.classList.remove('expanded'));
+
+    // Add 'expanded' to the selected card
     card.classList.add('expanded');
+
+    // Update price
     const price = radio.dataset.price;
     totalPriceDisplay.textContent = `$${price}.00 USD`;
   });
